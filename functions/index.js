5f12ca37abc3ed18b0f251deaf8c72f455cd0907
MP Harrison <martin@MPs-MacBook-Pro.local>
2020-08-08 14:39:19 +0100

    remove dist folder

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 const app = (0, _express.default)();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '5000';
 app.use((0, _morgan.default)('dev'));
 app.use(_express.default.json());
 app.use(_express.default.urlencoded({
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import wordRoutes from "./routes/words.routes";
 import scoreRoutes from "./routes/score.routes";
 
 const app = express();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '5000';
 
 app.use(logger('dev'));
 app.use(express.json());
